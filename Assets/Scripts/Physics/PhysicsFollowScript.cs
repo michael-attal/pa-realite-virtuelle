c3cd4e48594fde9df1f42bf84204ab76251875ae
Chachoune963 <sacha@toutut.fr>
2024-03-20 16:18:55 +0100

    [ADD] Physical player and interactivity with bag

---
+++ Assets/Scripts/Physics/PhysicsFollowScript.cs
@@ -0,0 +1,33 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody))]
+public class PhysicsFollowScript : MonoBehaviour
+{
+    [SerializeField] private Rigidbody body;
+    [SerializeField] private Transform followedObject;
+
+    [SerializeField] private float followSpeed = 30f;
+    [SerializeField] private float followRotationSpeed = 100f;
+
+    private void Start()
+    {
+        transform.SetPositionAndRotation(followedObject.position, followedObject.rotation);
+    }
+
+    private void FixedUpdate()
+    {
+        // Track Position
+        var currentPosition = transform.position;
+        var followedPosition = followedObject.position;
+        var dist = Vector3.Distance(currentPosition, followedPosition);
+        body.velocity = (followedPosition - currentPosition).normalized * (followSpeed * dist);
+        
+        // Track Rotation
+        var rotation = followedObject.rotation * Quaternion.Inverse(body.rotation);
+        rotation.ToAngleAxis(out float angle, out Vector3 axis);
+        body.angularVelocity = axis * (angle * Mathf.Deg2Rad * followRotationSpeed);
+    }
+}
