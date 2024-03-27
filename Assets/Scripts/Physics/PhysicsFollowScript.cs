4cd694b2f3b4a357d38518fef33e08e3001a6cc7
Chachoune963 <sacha@toutut.fr>
2024-03-27 19:19:32 +0100

    [FIX] Teleportation area preventing collisions

--- Assets/Scripts/Physics/PhysicsFollowScript.cs
+++ Assets/Scripts/Physics/PhysicsFollowScript.cs
@@ -8,23 +8,29 @@ public class PhysicsFollowScript : MonoBehaviour
 
     [SerializeField] private float followSpeed = 1000f;
     [SerializeField] private float followRotationSpeed = 4000f;
-
-    private void Start()
-    {
-        transform.SetPositionAndRotation(followedObject.position, followedObject.rotation);
-    }
+    [SerializeField] private float distanceToReset = 1f;
 
     private void FixedUpdate()
     {
-        // Track Position
         var currentPosition = transform.position;
         var followedPosition = followedObject.position;
         var dist = Vector3.Distance(currentPosition, followedPosition);
-        body.velocity = (followedPosition - currentPosition).normalized * (followSpeed * dist * Time.fixedDeltaTime);
         
-        // Track Rotation
-        var rotation = followedObject.rotation * Quaternion.Inverse(body.rotation);
-        rotation.ToAngleAxis(out float angle, out Vector3 axis);
-        body.angularVelocity = axis * (angle * Mathf.Deg2Rad * followRotationSpeed * Time.fixedDeltaTime);
+        if (dist > distanceToReset)
+        {
+            // If hitbox is too far from current position, reset current position
+            // Have to be careful with this, it could let players go through walls...
+            transform.SetPositionAndRotation(followedObject.position, followedObject.rotation);
+        }
+        else
+        {
+            // Track Position
+            body.velocity = (followedPosition - currentPosition).normalized * (followSpeed * dist * Time.fixedDeltaTime);
+
+            // Track Rotation
+            var rotation = followedObject.rotation * Quaternion.Inverse(body.rotation);
+            rotation.ToAngleAxis(out float angle, out Vector3 axis);
+            body.angularVelocity = axis * (angle * Mathf.Deg2Rad * followRotationSpeed * Time.fixedDeltaTime);
+        }
     }
 }
