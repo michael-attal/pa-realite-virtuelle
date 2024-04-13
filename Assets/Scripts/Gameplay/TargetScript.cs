871e02fbd933e2422a51706717dde4266451d47d
Chachoune963 <sacha@toutut.fr>
2024-04-13 18:18:11 +0200

    [ADD] #24 Add combo system

--- Assets/Scripts/Gameplay/TargetScript.cs
+++ Assets/Scripts/Gameplay/TargetScript.cs
@@ -6,7 +6,8 @@ public class TargetScript : MonoBehaviour
 {
     private const int PrecisionBonus = 500;
     private const int PowerBonus = 500;
-    private const int PerfectHitThreshhold = 800;
+    private const int TimingBonus = 500;
+    private const int PerfectHitThreshhold = 1100;
 
     private const float MinimumSize = 0.25f;
     
@@ -20,10 +21,14 @@ public class TargetScript : MonoBehaviour
 
     [SerializeField] private float hitDistanceTolerance;
 
+    private float timingFactor;
+    private bool waitingForHit;
+
     private void Start()
     {
         GameObject soundManagerObject = GameObject.Find("SoundManager");
         soundManager = soundManagerObject.GetComponent<SoundManager>();
+        gameObject.SetActive(false);
     }
 
     private void OnTriggerEnter(Collider other)
@@ -38,31 +43,41 @@ public class TargetScript : MonoBehaviour
             // Check precision of hit
             var closest = other.ClosestPoint(idealHitPoint.position);
             var precisionFactor = 1 - Mathf.Clamp(Vector3.Distance(closest, idealHitPoint.position), 0, hitDistanceTolerance) / hitDistanceTolerance;
-
-            var totalPoints = (int)(PrecisionBonus * precisionFactor + PowerBonus * powerFactor);
+            
+            var totalPoints = (int)(PrecisionBonus * precisionFactor + PowerBonus * powerFactor + TimingBonus * timingFactor);
             if (totalPoints >= PerfectHitThreshhold)
                 foreach (var particles in onHitParticleSystems)
                     particles.Play();
 
+
             soundManager.PlayPunchSound(totalPoints >= PerfectHitThreshhold ? PunchType.Perfect : PunchType.Normal);
             ScoreManager.Instance.Score += totalPoints;
+            
+            gameObject.SetActive(false);
         }
     }
 
     public void StartCountdown(float seconds)
-        => StartCoroutine(CountdownHit(seconds));
+    {
+        gameObject.SetActive(true);
+        StartCoroutine(CountdownHit(seconds));
+    }
 
     private IEnumerator CountdownHit(float seconds)
     {
         float timing = 0;
         while (timing <= seconds)
         {
-            var coefficient = 1 - timing / seconds;
-            timingCircle.color = Color.Lerp(endColor, startColor, coefficient);
-            timingCircle.rectTransform.localScale = Vector3.one * Mathf.Lerp(MinimumSize, 1f, coefficient);
+            // TimingFactor is also used to calculate the points given at a given time
+            timingFactor = timing / seconds;
+            timingCircle.color = Color.Lerp(startColor, endColor, timingFactor);
+            timingCircle.rectTransform.localScale = Vector3.one * Mathf.Lerp(MinimumSize, 1f, 1 - timingFactor);
             
             timing += Time.deltaTime;
             yield return null;
         }
+
+        timingFactor = 0f;
+        gameObject.SetActive(false);
     }
 }
